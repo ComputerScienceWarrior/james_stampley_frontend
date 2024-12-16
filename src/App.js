2944814d7995c2437910e5105c39b6f33ad92b1b
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-16 08:16:33 -0700

    adds in routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Resume from './components/Resume/Resume';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
     </div>
   );
