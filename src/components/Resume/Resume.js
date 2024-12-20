73d0f6ee083cd608826a8f1f25496ccdc3aa6b25
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-20 07:29:13 -0700

    adds headerInfo and professional summary to Resume component

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,25 @@
 import React from "react";
+import './Resume.css';
+import { summaryText } from "../extras/AboutMeText";
 
 const Resume = () => {
     return(
-        <div>
-            Resume
-        </div>
+        <section className="resumeContainer">
+            <section>
+                <section className="headerInfo">
+                    <section>James-Ryan Stampley</section>
+                    <section>(602)-374-0977</section>
+                    <section>software.engineer.ryan@gmail.com</section>
+                </section>
+                <section className="summary">
+                    <section className="titleHeader">Professional Summary</section>
+                    <section className="summaryBody">{summaryText}</section>
+                </section>
+                <section className="workExperience">
+                    <section className="titleHeader">Work Experience</section>
+                </section>
+            </section>
+        </section>
     );
 };
 
