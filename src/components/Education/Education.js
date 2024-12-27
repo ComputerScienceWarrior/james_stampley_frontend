f887dfea70a53e1ea8b45f2b711537a8c87b0b4f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-27 09:09:52 -0700

    conditionally display education information bullets

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import './Education.css';
 
 const Eduaction = (props) => {
+    const [viewMore, setViewMore] = useState(false);
+    const changeView = () => {
+        setViewMore(!viewMore);
+    };
+
     return(
         <section className="educationWrapper">
             <section className="schoolNameAndDatesAttended">
@@ -9,12 +14,25 @@ const Eduaction = (props) => {
                 <section className="">{props.yearsAttended}</section>
             </section>
             <section><b>{props.degree}</b></section>
-            <ul>
-                {props.bullet1 && ( <li>{props.bullet1}</li> ) }
-                {props.bullet2 && ( <li>{props.bullet2}</li> ) }
-                {props.bullet3 && ( <li>{props.bullet3}</li> ) }
-                {props.bullet4 && ( <li>{props.bullet4}</li> ) }
-            </ul>
+            <button className="viewMoreToggle" onClick={changeView}>
+                {
+                    viewMore 
+                        ?
+                            <img alt="img" className="imageArrow" src="/arrow-up.png"/>
+                        : 
+                            <img alt="img" className="imageArrow" src="/drop-down-arrow.png"/>
+                }
+            </button>
+            {
+                viewMore && (
+                    <ul>
+                        {props.bullet1 && ( <li>{props.bullet1}</li> ) }
+                        {props.bullet2 && ( <li>{props.bullet2}</li> ) }
+                        {props.bullet3 && ( <li>{props.bullet3}</li> ) }
+                        {props.bullet4 && ( <li>{props.bullet4}</li> ) }
+                    </ul>
+                )
+            }
         </section>
     );
 };
