1375e363c09e7205384105c8bab957f5f894845a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2024-12-27 22:11:34 -0700

    adds react route dom navigation capabilities

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './Project.css';
+import { useNavigate } from "react-router-dom";
 
 const Project = (props) => {
+    const navigate = useNavigate();
+
     return(
         <section className="projectWrapper">
             <header className="projectCardName"><b>{props.name}</b></header>
             <img className="cardImage" src={props.imageSource} />
+            <button onClick={() => navigate(`/project/${props.id}`, {state: {name: props.name, image: props.imageSource, id: props.id, caption: props.caption }})}>View Project</button>
             <section className="cardCaption">{props.caption}</section>
         </section>
     );
