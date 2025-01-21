fda84c4ad08ba6e3e56326dc0609b8fbc86ddb1f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-01-21 08:16:10 -0700

    creates external ProjectData for import use

---
+++ src/components/extras/ProjectData.js
@@ -0,0 +1,18 @@
+const ProjectData = [
+    {
+        id : "1",
+        name: "Fitness Mobile App",
+        caption: "A fitness Mobile Application to help you along the way of your fitness journey.", 
+        imageSource: "https://placehold.co/600x400",
+        language: 'JavaScript, React Native, Ruby on Rails'
+    },
+    {
+        id : "2",
+        name: "Inventory Management System",
+        caption: "A system for keeping track of inventory.", 
+        imageSource: "https://placehold.co/600x400",
+        language: 'C#, .NET Framework'
+    }
+]
+
+export default ProjectData;
