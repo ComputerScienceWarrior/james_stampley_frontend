778a260dccdaad00c20642f25ce1ab7d61578fad
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-01-21 08:15:31 -0700

    adds in Swiper component to cycle through projects on Home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,43 @@
 import React from "react";
 import './Home.css';
-import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import ProjectData from "../extras/ProjectData";
 
 const Home = () => {
-    return(
+    return (
         <div className="container">
             <header className="header">
-                <p className="headerText">
-                    James-Ryan Stampley's Software Engineering Portfolio
-                </p>
-                <section className="linksWrapper">
-                    <button className="buttons" id="aboutButton"><Link to='/about'>About</Link></button>
-                    <button className="buttons" id="contactButton"><Link to='/contact'>Contact Me</Link></button>
-                    <button className="buttons" id="projectsButton"><Link to='/projects'>View Projects</Link></button>
-                    <button className="buttons" id="resumeButton"><Link to='/resume'>View Resume</Link></button>
-                </section>
+                <Swiper
+                    modules={[Navigation, Pagination, Autoplay]}
+                    spaceBetween={20}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    autoplay={{ delay: 3000 }}
+                    loop
+                    className="mySwiper"
+                >
+                    {ProjectData.map((project) => (
+                        <SwiperSlide key={project.id}>
+                            <div className="card">
+                                <img
+                                    src={project.imageSource}
+                                    alt={`${project.name} preview`}
+                                    className="card-image"
+                                />
+                                <h3 className="card-title">{project.name}</h3>
+                                <p className="card-caption">{project.caption}</p>
+                                <p className="card-language">
+                                    <strong>Languages/Technologies:</strong> {project.language}
+                                </p>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </header>
         </div>
     );
