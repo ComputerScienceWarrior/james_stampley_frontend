7a0ef1290128b585b21dfbf7054c3d82fc9b8977
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-01-21 08:16:56 -0700

    adds in section for programming Language used

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -9,8 +9,9 @@ const Project = (props) => {
         <section className="projectWrapper">
             <header className="projectCardName"><b>{props.name}</b></header>
             <img className="cardImage" src={props.imageSource} />
-            <button onClick={() => navigate(`/project/${props.id}`, {state: {name: props.name, image: props.imageSource, id: props.id, caption: props.caption }})}>View Project</button>
+            <button onClick={() => navigate(`/project/${props.id}`, {state: {name: props.name, image: props.imageSource, id: props.id, caption: props.caption, language: props.language }})}>View Project</button>
             <section className="cardCaption">{props.caption}</section>
+            <section className="language">{props.language}</section>
         </section>
     );
 };
