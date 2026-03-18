929da8120489728c75a278005815c121bad3c456
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2026-03-18 16:32:24 -0700

    updates style and structure of 'About' component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,16 @@
-import React from "react";
 import './About.css';
 import { aboutMeText } from "../extras/TextBodies";
 
 const About = () => {
     return (
-        <section className="wrapper">
-            <header className="aboutHeader">
-                A little about James-Ryan Stampley...
-            </header>
-            <section className="body">
-                <p>{aboutMeText}</p>
-            </section>
+        <section className="about-container">
+            <div className="about-card">
+                <h1 className="about-title">A little about James-Ryan Stampley...</h1>
+                <div className="about-divider"></div>
+                <section className="about-body">
+                    <p>{aboutMeText}</p>
+                </section>
+            </div>
         </section>
     );
 };
