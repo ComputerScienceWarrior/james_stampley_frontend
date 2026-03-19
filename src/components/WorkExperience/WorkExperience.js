06bf0aa4e95c798b4a98aec576c18d111cbce942
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2026-03-19 07:09:12 -0700

    updates work experience component to fit with site theme

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -1,36 +1,40 @@
 import React, { useState } from "react";
-import './WorkExperience.css';
+import "./WorkExperience.css";
 
-const WorkExperience = ( props ) => {
-    const [viewMore, setViewMore] = useState(false);
+const WorkExperience = (props) => {
+  const [viewMore, setViewMore] = useState(false);
 
-    const changeView = () => {
-        setViewMore(!viewMore)
-    }
-    return (
-        <section className="workExperienceSummaries">
-            <section className="date">{props.jobName} - <b>{props.jobTitle}</b>
-                <button className="viewMoreToggle" onClick={changeView}>
-                    { viewMore 
-                        ?
-                            <img alt="img" className="imageArrow" src="/arrow-up.png"/>
-                        : 
-                            <img alt="img" className="imageArrow" src="/drop-down-arrow.png"/>
-                    } 
-                </button>
-            </section>
-            {
-                viewMore && (
-                    <ul>
-                        { props.bullet1 && ( <li>{props.bullet1}</li> ) }
-                        { props.bullet2 && ( <li>{props.bullet2}</li> ) }
-                        { props.bullet3 && ( <li>{props.bullet3}</li> ) }
-                        { props.bullet4 && ( <li>{props.bullet4}</li> ) }
-                    </ul>
-                )
-            }
-        </section>
-    );
+  return (
+    <section className={`work-card ${viewMore ? "open" : ""}`}>
+      
+      <div className="work-header" onClick={() => setViewMore(!viewMore)}>
+        
+        <div className="work-main">
+          <h3 className="work-job">{props.jobTitle}</h3>
+          <p className="work-company">{props.jobName}</p>
+        </div>
+
+        <div className="work-meta">
+          <span className="work-date">{props.date}</span>
+
+          <span className="work-toggle">
+            {viewMore ? "−" : "+"}
+          </span>
+        </div>
+
+      </div>
+
+      <div className={`work-content ${viewMore ? "show" : ""}`}>
+        <ul>
+          {props.bullet1 && <li>{props.bullet1}</li>}
+          {props.bullet2 && <li>{props.bullet2}</li>}
+          {props.bullet3 && <li>{props.bullet3}</li>}
+          {props.bullet4 && <li>{props.bullet4}</li>}
+        </ul>
+      </div>
+
+    </section>
+  );
 };
 
 export default WorkExperience;
